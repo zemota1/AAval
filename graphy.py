b1c178a93ceb934c2d6407eee918102fa6d1df54
zemota1 <zemariamota@hotmail.com>
2018-04-10 15:22:37 +0100

    Prim algorith with graph struct

--- graphy.py
+++ graphy.py
@@ -1,92 +1,129 @@
 class Edge:
-	def __init__(self, source, dest, weight):
+	def __init__(self, source, dest, val):
 		self.source = source
-		self.dest = dest
-		self.weight = weight
-	
-
-class Node:
-	def __init__(self, val):
-		self.parent = None
 		self.val = val
+		self.dest = dest
 		self.next = None
-	
+
 	def getVal(self):
 		return self.val
 
-	def getNext(self):
-		return self.next
-	
-	def getParent(self):
-		return self.parent
-	
+	def getDest(self):
+		return self.dest
+
+	def getSource(self):
+		return self.source
+
 	def setVal(self, val):
 		self.val = val
 
+	def setDest(self, dest):
+		self.dest = dest
+
+	def setSource(self, source):
+		self.source = source
+
+	def getNext(self):
+		return self.next
+
 	def setNext(self, next):
-		self.next = next
-	
-	def setParent(self, parent):
-		self.parent = parent
-		
+		self.next = next				
+
 class LinkedList:
 	def __init__(self):
 		self.head = None
-		
+
 	def getHead(self):
 		return self.head
-	
+
 	def setHead(self, head):
-		self.head = head	
-	
+		self.head = head
+
 	def append(self, item):
 		current = self.head
 		if current:
 			while current.getNext()!= None:
 				current = current.getNext()
-			current.setNext(Node(item))	
+			current.setNext(item)
 		else:
-			self.head = Node(item)
-	
+			self.head = item
+
 	def hasElem(self, val):
 		current = self.head
 		if current:
 			while current.getNext()!= None:
 				if(current.getNext().getVal(val) == val):
 					return True
-				current = current.getNext()	
-			return False	
+				current = current.getNext()
+			return False
 		return False
-	
+
 	def displayList(self):
 		current = self.head
 		if current:
 			while current.getNext()!= None:
 				print(current.getVal())
-				current = current.getNext()	
-			print(current.getVal())	
-			
-					
-		
+				current = current.getNext()
+			print(current.getVal())
+
+
+
 class Graph:
 	def __init__(self, size):
 		self.links = [LinkedList]*size
+		self.size = size
 	def getLinksOfVertexV(self, vertexId):
-		return self.links[vertexId]	
+		return self.links[vertexId]
 
 
 
-g = Graph(10)
+g = Graph(6)
+n1 = Edge('A', 'B', 4)
+n11 = Edge('B', 'A', 4)
+n2 = Edge('A', 'F', 2)
+n22 = Edge('F', 'A', 2)
+n3 = Edge('B', 'C', 6)
+n33 = Edge('C', 'B', 6)
+n4 = Edge('B', 'F', 3)
+n44 = Edge('F', 'B', 3)
+n5 = Edge('C', 'F', 1)
+n55 = Edge('F', 'C', 1)
+n6 = Edge('C', 'D', 3)
+n66 = Edge('D', 'C', 3)
+n8 = Edge('D', 'E', 2)
+n88 = Edge('E', 'D', 2)
+n9 = Edge('F', 'E', 4)
+n99 = Edge('E', 'F', 4)
+
 l1 = LinkedList()
-l1.append(10)
-l1.append(20)
-l1.append(30)
+l1.append(n1)
+l1.append(n2)
 l2 = LinkedList()
-l2.append(70)
-l2.append(20)
-l2.append(30)
-l2.append(50)
-l2.append(40)
+l2.append(n11)
+l2.append(n3)
+l2.append(n4)
+l3 = LinkedList()
+l3.append(n33)
+l3.append(n5)
+l3.append(n6)
+l4 = LinkedList()
+l4.append(n66)
+l4.append(n8)
+l5 = LinkedList()
+l5.append(n88)
+l5.append(n99)
+l6= LinkedList()
+l6.append(n44)
+l6.append(n55)
+l6.append(n9)
+
+
+
 g.links[0] = l1
 g.links[1] = l2
-g.getLinksOfVertexV(1).displayList()
+g.links[2] = l3
+g.links[3] = l4
+g.links[4] = l5
+g.links[5] = l6
+
+
--- prim.py
+++ prim.py
@@ -1,35 +1,101 @@
 import math
+import graphy
 
-def prim(root, n):
+def prim(root, g):
 
+    n = g.size
     a = []
     key = [(math.inf)]*n #numero de vertices
     parent = [(None)]*n
-
-    #test
-    adj = {0: [(1,4),(5,2)], 1: [(0,4),(2,6),(5,3)], 2: [(1,6), (3,3), (5,1)], 3: [(2,3),(4, 2)], 4: [(3,2), (5,4)], 5: [(0,2), (1,3), (2,1), (4, 4)]} #a:0 ; b:1 ; c:2 ; d:3 ; e: 4 ; f:5
-    vertices = [0, 1, 2, 3, 4 ,5] #
     key[root] = 0
+    vertices = [0, 1, 2, 3, 4 ,5]
+
+  
 
     while (len(vertices) != 0):
 
             u = key.index(min(key)) #vamos utilozar hip?
-            key[u] = math.inf
-            print(key)
-            print(u)
+            print('Key', key)
+            print('Vertices', vertices)
+            print('u min', u)
             vertices.remove(u)
-
-
-            print ('Depois do remove' , vertices)
+            key[u] = math.inf
+            print('vertices remove u', vertices)
 
             if (parent[u] != None):
                 a.append((u, parent[u]))
 
-            for i in adj.get(u):
-                if (i[0] in vertices) and (i[1] < key[i[0]]):
-                    parent[i[0]] = u
-                    key[i[0]] = i[1]
+            adj = g.getLinksOfVertexV(u)
+            #adj.displayList()
+            aux = adj.getHead()
+            while (aux != None):
+                d = aux.getDest()
+                v = aux.getVal()
+                print('D', d)
+                print('V', v)
+                if (d in vertices) and (v < key[d]):
+                    parent[d] = u
+                    key[d] = v
+                print('parent', parent)
+                print('Key', key)
+                if (aux.getNext() == None):
+                    break;
+                else:        
+                    aux = aux.getNext()
+
+                print('---------------')
+            print('§§§§§§§§§§§§') 
 
     print(a)
 
-prim(0, 6)
+g = graphy.Graph(6)
+n1 = graphy.Edge(0, 1, 4)
+n11 = graphy.Edge(1, 0, 4)
+n2 = graphy.Edge(0, 5, 2)
+n22 = graphy.Edge(5, 0, 2)
+n3 = graphy.Edge(1, 2, 6)
+n33 = graphy.Edge(2, 1, 6)
+n4 = graphy.Edge(1, 5, 3)
+n44 = graphy.Edge(5, 1, 3)
+n5 = graphy.Edge(2, 5, 1)
+n55 = graphy.Edge(5, 2, 1)
+n6 = graphy.Edge(2, 3, 3)
+n66 = graphy.Edge(3, 2, 3)
+n8 = graphy.Edge(3, 4, 2)
+n88 = graphy.Edge(4, 3, 2)
+n9 = graphy.Edge(5, 4, 4)
+n99 = graphy.Edge(4, 5, 4)
+
+l1 = graphy.LinkedList()
+l1.append(n1)
+l1.append(n2)
+l2 = graphy.LinkedList()
+l2.append(n11)
+l2.append(n3)
+l2.append(n4)
+l3 = graphy.LinkedList()
+l3.append(n33)
+l3.append(n5)
+l3.append(n6)
+l4 = graphy.LinkedList()
+l4.append(n66)
+l4.append(n8)
+l5 = graphy.LinkedList()
+l5.append(n88)
+l5.append(n99)
+l6 = graphy.LinkedList()
+l6.append(n44)
+l6.append(n55)
+l6.append(n9)
+
+
+
+g.links[0] = l1
+g.links[1] = l2
+g.links[2] = l3
+g.links[3] = l4
+g.links[4] = l5
+g.links[5] = l6
+
+prim(0,g)
+
